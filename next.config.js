81831619686e78b0a6639d558b215e1cd64683e3
Naveen Kumarasinghe <dndkumarasinghe@gmail.com>
2023-08-29 12:20:31 +0530

    feat(boilerplate): enable Next.js server actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // TODO: remove experimental flags when stabilized
     experimental: {
         instrumentationHook: true,
+        serverActions: true,
     },
 };
 
