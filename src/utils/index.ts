2dbffd6b83e0a3fd44e18781be77090ade7620be
Naveen Kumarasinghe <dndkumarasinghe@gmail.com>
2023-08-30 13:59:45 +0530

    feat(boilerplate): configure shadcn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./font-class";
+export * from "./cn";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,71 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{ts,tsx}"],
     theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         extend: {
             colors: {
-                foreground: "var(--color-foreground)",
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
                 accent: {
-                    DEFAULT: "var(--color-accent)",
-                    light: "var(--color-accent-light)",
-                    dark: "var(--color-accent-dark)",
-                    foreground: "var(--color-accent-foreground)",
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
                 },
-                base: {
-                    DEFAULT: "var(--color-base)",
-                    50: "var(--color-base-50)",
-                    100: "var(--color-base-100)",
-                    200: "var(--color-base-200)",
-                    300: "var(--color-base-300)",
-                    400: "var(--color-base-400)",
-                    500: "var(--color-base-500)",
-                    600: "var(--color-base-600)",
-                    700: "var(--color-base-700)",
-                    800: "var(--color-base-800)",
-                    900: "var(--color-base-900)",
-                    950: "var(--color-base-950)",
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
                 },
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
